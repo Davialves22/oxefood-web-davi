202f8b6eec2af3dc051d64f90a3cc8933df95035
Davi <dgas1@discente.ifpe.edu.br>
2025-04-12 14:14:57 -0300

    #

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Segment } from 'semantic-ui-react';
 import Home from './views/home/Home';
 
 function App() {
   return (
     <div className="App">
+
       <Home />
+
+      <div style={{ marginTop: '6%' }}>
+        <Segment vertical color='grey' size='tiny' textAlign='center'>
+          &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
+        </Segment>
+      </div>
+
     </div>
   );
 }
