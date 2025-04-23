bb9afe4be8b84ed64772bbf6e10b4bafa99b3128
Davi <dgas1@discente.ifpe.edu.br>
2025-04-23 19:23:04 -0300

    colocado axios

--- src/views/cliente/FormCLiente.jsx
+++ src/views/cliente/FormCLiente.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import InputMask from "comigo-tech-react-input-mask";
 import { Button, Container, Divider, Form, Icon } from "semantic-ui-react";
 import MenuSistema from "../../MenuSistema";
+import axios from "axios";
 
 export default function FormCliente() {
   const [nome, setNome] = useState();
@@ -9,6 +10,25 @@ export default function FormCliente() {
   const [dataNascimento, setDataNascimento] = useState();
   const [foneCelular, setFoneCelular] = useState();
   const [foneFixo, setFoneFixo] = useState();
+
+  function salvar() {
+    let clienteRequest = {
+      nome: nome,
+      cpf: cpf,
+      dataNascimento: dataNascimento,
+      foneCelular: foneCelular,
+      foneFixo: foneFixo,
+    };
+
+    axios
+    .post("http://localhost:8080/api/cliente", clienteRequest)
+      .then((response) => {
+        console.log("Cliente cadastrado com sucesso.");
+      })
+      .catch((error) => {
+        console.log("Erro ao incluir o um cliente.");
+      });
+  }
 
   return (
     <div>
